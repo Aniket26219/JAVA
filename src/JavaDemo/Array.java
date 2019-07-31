6d43349ae9294d03a5960e71cd515ebba82b34a1
ANIKET <aniket98shahane@gmail.com>
2019-07-31 13:34:17 +0530

    1 ST JAVA

--- src/JavaDemo/Array.java
+++ src/JavaDemo/Array.java
@@ -10,10 +10,24 @@ public class Array {
         for(int i=0;i<6;i++){
             System.out.println("Array :-"+array[i]);
         }*/
-        int array[][] = new int[5][5];
         Scanner sc = new Scanner(System.in);
+        int row=0;
+        int column=0;
+        System.out.println("Enter no. of columns and rows :-");
+        row=sc.nextInt();
+        column=sc.nextInt();
+        int array[][] = new int[column][row];
         for (int i = 0; i < array.length; i++) {
-
+            for(int j=0;j<array.length;j++){
+                System.out.println("Enter the value of array of "+i+" of "+j+" :-");
+                array[i][j]=sc.nextInt();
+            }
+        }
+        for (int i=0;i<array.length;i++){
+            for (int j=0;j<array.length;j++){
+                System.out.print(array[i][j]+"\t");
+            }
+            System.out.println();
         }
     }
 }
